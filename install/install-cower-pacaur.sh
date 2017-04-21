557c8563dd1b76fdb42d1f03d250778c908c9e77
Dan Alec Yamaguchi <danalec@gmail.com>
2017-04-21 15:42:53 -0300

    fix derp

--- install/install-cower-pacaur.sh
+++ install/install-cower-pacaur.sh
@@ -7,25 +7,22 @@ gpg --keyserver pgp.mit.edu --recv-keys 487EACC08557AD082088DABA1EB2638FF56C0C53
 gpg --keyserver pgp.mit.edu --recv-keys 1EB2638FF56C0C53
 
 echo 'creating temporary folder'
-TMPCOWERDIR="${0##*/}-$$"
-mkdir -v "/tmp/$TMPCOWERDIR"
-TMPPACAURDIR="${0##*/}-$$"
-mkdir -v "/tmp/$TMPPACAURDIR"
+TMPDIR="${0##*/}-$$"
+mkdir -v "/tmp/$TMPDIR"
 
 echo 'downloading cower'
-cd /tmp/$TMPCOWERDIR || exit 1
+cd /tmp/$TMPDIR || exit 1
 curl -L https://aur.archlinux.org/cgit/aur.git/snapshot/cower.tar.gz | tar xz
 
 echo 'cower makepkg'
 makepkg --syncdeps --install --noconfirm
 
 echo 'downloading pacaur'
-cd /tmp/$TMPPACAURDIR || exit 1
+cd /tmp/$TMPDIR || exit 1
 curl -L https://aur.archlinux.org/cgit/aur.git/snapshot/pacaur.tar.gz | tar xz
 
 echo 'pacaur makepkg'
 makepkg --syncdeps --install --noconfirm
 
 echo 'cleaning temporary folder'
-rm -rf "/tmp/$TMPCOWERDIR"
-rm -rf "/tmp/$TMPPACAURDIR"
+rm -rf "/tmp/$TMPDIR"
